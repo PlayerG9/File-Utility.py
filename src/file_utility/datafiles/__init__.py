aaa2d6a5aec9fd85fc721281d64e4774c0758ede
PlayerG9 <g9yout@gmail.com>
2021-12-15 22:48:43 +0100

    added comments for what these sub-packages should do

--- src/file_utility/datafiles/__init__.py
+++ src/file_utility/datafiles/__init__.py
@@ -1,4 +1,19 @@
 # -*- coding=utf-8 -*-
 r"""
+open files like .json .ini and more process-same
 
+every time you open a file it's the same object
+
+a = open_datafile('./data.json')
+b = open_datafile('./data.json')
+
+print(a['key'])  # 'value'
+print(b['key'])  # 'value'
+
+a['key'] = 'new'
+
+print(a['key'])  # 'new'
+print(b['key'])  # 'new'
+
+print(a is b)  # True
 """
--- src/file_utility/synced_datafiles/__init__.py
+++ src/file_utility/synced_datafiles/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding=utf-8 -*-
 r"""
-
+same as file_utility.datafiles but the object automatically loads and saves changes in and from the file
 """
