a2376ed3c176f717449911a7db3be2d9d6895785
PlayerG9 <g9yout@gmail.com>
2021-12-21 17:20:22 +0100

    added .add() and ._jump_to()

--- src/file_utility/datafiles/pyobjfile.py
+++ src/file_utility/datafiles/pyobjfile.py
@@ -46,20 +46,26 @@ class PyObjFile(FileBase):
         with self._get_file() as file:
             file: io.BufferedIOBase
             self._jump_to(file, index)  # go to index
-            bytes_size = self._read_bsize(file)
-            object_bytes = file.read(bytes_size)  # read the size
+            object_size = self._read_bsize(file)  # get size of object
+            object_bytes = file.read(object_size)  # read the size
             return pickle.loads(object_bytes)  # convert back / load object
 
-    def add(self, value) -> int:
+    def add(self, obj: object):
+        with self._get_file() as file:
+            file.seek(0, os.SEEK_END)  # go to the end
+            object_bytes = pickle.dumps(obj)  # dump object
+            object_size = len(object_bytes)  # get size of object
+            bytes_size = object_size.to_bytes(2, 'little', signed=True)  # convert size to bytes
+            file.write(bytes_size)  # write size
+            file.write(object_bytes)  # write object/bytes
+    
+    def delete(self, index: int = None):
         with self._get_file() as file:
             pass
     
-    def delete(self, index: int = None):
-        with open(self.filepath, 'ab'):  # does this clear the file
-            pass
-    
     def delete_many(self, indezies: list):
-        pass
+        with self._get_file() as file:
+            pass
 
     ####################################################################################################################
     
@@ -75,9 +81,12 @@ class PyObjFile(FileBase):
             raise OSError('invalid file-content')
         return file
     
-    @staticmethod
-    def _jump_to(file: io.BufferedIOBase, index: int):
-        pass
+    def _jump_to(self, file: io.BufferedIOBase, index: int):
+        file.seek(len(MAGIC_NUMBER))  # go to first
+        for _ in range(index):
+            object_size = self._read_bsize(file)
+            next_index = file.tell() + object_size
+            file.seek(next_index)
     
     @staticmethod
     def _read_bsize(file) -> int:
